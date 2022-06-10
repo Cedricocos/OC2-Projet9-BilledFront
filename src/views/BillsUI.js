1bb02290cad4f6dec4342f3331b47847570407c7
Cédric <cedric.olivry@sfr.fr>
2022-06-10 13:16:29 +0200

    fix sorted bill bugs on bills pages

--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -20,7 +20,10 @@ const row = (bill) => {
   }
 
 const rows = (data) => {
-  return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
+  const antiChrono = (a, b) => ((a.date < b.date) ? 1 : -1)
+  const dataSorted = [...data].sort(antiChrono)
+  
+  return (dataSorted && dataSorted.length) ? dataSorted.map(bill => row(bill)).join("") : ""
 }
 
 export default ({ data: bills, loading, error }) => {
